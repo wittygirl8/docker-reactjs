4ca98d272e4350e9e811bf41f0544e01b49dff50
Riddhi K Singh <riddhi.singh@in.ey.com>
2024-10-20 06:40:07 +0530

    adding robot.txt

--- src/components/main-container.js
+++ src/components/main-container.js
@@ -65,7 +65,7 @@ export default class MainContainer extends Component {
         <div className="row">
           <div className="col s12">
             <div className="section">
-              <h3 className="header">Cryptocurrency Monitor!!!!!</h3>
+              <h3 className="header">____Cryptocurrency Monitor!!!!!</h3>
             </div>
           </div>
         </div>
